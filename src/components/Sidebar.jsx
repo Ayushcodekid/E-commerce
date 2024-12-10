992d61b3421cca1592aead297d17b494b577293e
Ayushcodekid <ayush0101works@gmail.com>
2024-12-10 17:32:40 +0530

    transition

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,50 +20,51 @@ const Sidebar = () => {
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 ';
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {activeMenu && (
-        <>
-          <div className="flex justify-between items-center">
-            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-            <img style={{height: '4vh'}} src={Logo}></img>
-              <span>Shoppy</span>
-            </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
-              <button
-                type="button"
-                onClick={() => setActiveMenu(!activeMenu)}
-                style={{ color: currentColor }}
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-              >
-                <MdOutlineCancel />
-              </button>
-            </TooltipComponent>
-          </div>
-          <div className="mt-10 ">
-            {links.map((item) => (
-              <div key={item.title}>
-                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
-                  {item.title}
-                </p>
-                {item.links.map((link) => (
-                  <NavLink
-                    to={`/${link.name}`}
-                    key={link.name}
-                    onClick={handleCloseSideBar}
-                    style={({ isActive }) => ({
-                      backgroundColor: isActive ? currentColor : '',
-                    })}
-                    className={({ isActive }) => (isActive ? activeLink : normalLink)}
-                  >
-                    {link.icon}
-                    <span className="capitalize ">{link.name}</span>
-                  </NavLink>
-                ))}
-              </div>
-            ))}
-          </div>
-        </>
-      )}
+    <div
+      className={`fixed top-0 left-0 h-screen bg-white dark:bg-secondary-dark-bg z-50 shadow-lg transition-all duration-300 ${activeMenu ? 'w-64' : 'w-0'}`}>      
+    {activeMenu && (
+      <>
+        <div className="flex justify-between items-center">
+          <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <img style={{ height: '4vh' }} src={Logo}></img>
+            <span>Shoppy</span>
+          </Link>
+          <TooltipComponent content="Menu" position="BottomCenter">
+            <button
+              type="button"
+              onClick={() => setActiveMenu(!activeMenu)}
+              style={{ color: currentColor }}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <MdOutlineCancel />
+            </button>
+          </TooltipComponent>
+        </div>
+        <div className="mt-10 ">
+          {links.map((item) => (
+            <div key={item.title}>
+              <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                {item.title}
+              </p>
+              {item.links.map((link) => (
+                <NavLink
+                  to={`/${link.name}`}
+                  key={link.name}
+                  onClick={handleCloseSideBar}
+                  style={({ isActive }) => ({
+                    backgroundColor: isActive ? currentColor : '',
+                  })}
+                  className={({ isActive }) => (isActive ? activeLink : normalLink)}
+                >
+                  {link.icon}
+                  <span className="capitalize ">{link.name}</span>
+                </NavLink>
+              ))}
+            </div>
+          ))}
+        </div>
+      </>
+    )}
     </div>
   );
 };
