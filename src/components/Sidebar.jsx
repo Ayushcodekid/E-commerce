e2634da23c037eec00925554e6dd5e2aa41f92bc
Ayushcodekid <ayush0101works@gmail.com>
2024-12-10 15:17:28 +0530

    logo added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import Logo from "../data/logo.png"
 
 import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -25,7 +25,8 @@ const Sidebar = () => {
         <>
           <div className="flex justify-between items-center">
             <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-              <SiShopware /> <span>Shoppy</span>
+            <img style={{height: '4vh'}} src={Logo}></img>
+              <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
